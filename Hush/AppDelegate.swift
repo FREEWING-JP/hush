ca773aae411dac313f1b79c2a6e219a91605d365
Nathan Dinsmore <nathan@users.noreply.github.com>
2015-07-23 20:12:28 -0400

    Initial Commit

---
+++ Hush/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hush
+//
+//  Created by Nathan Dinsmore on 2015-07-23.
+//  Copyright (c) 2015 Nathan Dinsmore. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
