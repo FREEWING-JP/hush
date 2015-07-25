ae02a8dc11e56bb44b0bab14dd12dca353c26df9
Nathan Dinsmore <nathan@users.noreply.github.com>
2015-07-25 16:11:36 -0400

    Move hashing stuff to a new class

--- Hush/AppDelegate.swift
+++ Hush/AppDelegate.swift
@@ -283,97 +283,15 @@ extension AppDelegate {
 
 extension AppDelegate {
   func generateHash() -> String? {
-    let pass = passField.stringValue
-    let tag = tagField.stringValue
-    guard
-      pass != "" && tag != "",
-      let passData = pass.dataUsingEncoding(NSASCIIStringEncoding),
-      let tagData = tag.dataUsingEncoding(NSASCIIStringEncoding) else {return nil}
-
-    let outPtr = UnsafeMutablePointer<Void>.alloc(Int(CC_SHA1_DIGEST_LENGTH))
-    CCHmac(CCHmacAlgorithm(kCCHmacAlgSHA1), passData.bytes, passData.length, tagData.bytes, tagData.length, outPtr)
-    let outData = NSData(bytes: outPtr, length: Int(CC_SHA1_DIGEST_LENGTH))
-    guard let b64 = NSString(data: outData.base64EncodedDataWithOptions([]), encoding: NSUTF8StringEncoding) as? String else {return nil}
-
-//    print(b64)
-    let seed = b64.utf8.reduce(0) {$0 + Int($1)} - (b64.characters.last == "=" ? Int("=".utf8.first!) : 0)
-//    print("\(b64.utf8.map {Int($0)}) \(seed)")
-    let str = b64[b64.startIndex..<advance(b64.startIndex, lengthButton.selectedTag())]
-    var utf = Array(str.utf8)
-
-    if onlyDigits.checked {
-      convertToDigits(&utf, seed: seed)
-    } else {
-      if requireDigit.checked {
-        injectChar(&utf, at: 0, seed: seed, from: 0x30, length: 10)
-      }
-      if requireSpecial.checked && !forbidSpecial.checked {
-        injectChar(&utf, at: 1, seed: seed, from: 0x21, length: 15)
-      }
-      if requireMixed.checked {
-        injectChar(&utf, at: 2, seed: seed, from: 0x41, length: 26)
-        injectChar(&utf, at: 3, seed: seed, from: 0x61, length: 26)
-      }
-      if forbidSpecial.checked {
-        stripSpecial(&utf, seed: seed)
-      }
-    }
-
-    var result = ""
-    for x in utf { result += String(Character(UnicodeScalar(x))) }
-    return result
-  }
-  func convertToDigits(inout utf: [UTF8.CodeUnit], seed: Int) {
-    // logic error in original:
-    var index = 0
-    var digit = false
-    for (i, x) in utf.enumerate() {
-      if 0x30 <= x && x <= 0x39 {
-        if !digit {index = i}
-        digit = true
-        continue
-      }
-      utf[i] = UTF8.CodeUnit(0x30 + (seed + Int(utf[index])) % 10)
-      index = i + 1
-      digit = false
-    }
-
-    // correct implementation:
-    // for (i, x) in utf.enumerate() {
-    //   if 0x30 <= x && x <= 0x39 {continue}
-    //   utf[i] = UTF8.CodeUnit(0x30 + (seed + Int(x)) % 10)
-    // }
-  }
-  var reservedChars: Int {get {return 4}}
-  func injectChar(inout utf: [UTF8.CodeUnit], at offset: Int, seed: Int, from start: UTF8.CodeUnit, length: UTF8.CodeUnit) {
-    let pos = (seed + offset) % utf.count
-    for i in 0..<utf.count - reservedChars {
-      let x = utf[(seed + reservedChars + i) % utf.count]
-      if start <= x && x <= start + length {return}
-    }
-    utf[pos] = UTF8.CodeUnit(Int(start) + (seed + Int(utf[pos])) % Int(length))
-  }
-  func stripSpecial(inout utf: [UTF8.CodeUnit], seed: Int) {
-    // TODO: very similar to convertToDigits, but awkwardly different
-    // logic error in original:
-    var index = 0
-    var special = true
-    for (i, x) in utf.enumerate() {
-      if 0x30 <= x && x <= 0x39 || 0x41 <= x && x <= 0x5a || 0x61 <= x && x <= 0x7a {
-        if special {index = i}
-        special = false
-        continue
-      }
-      utf[i] = UTF8.CodeUnit(0x41 + (seed + index) % 26)
-      index = i + 1
-      special = true
-    }
-
-    // correct implementation:
-    // for (i, x) in utf.enumerate() {
-    //   if 0x30 <= x && x <= 0x39 || 0x41 <= x && x <= 0x5a || 0x61 <= x && x <= 0x7a {continue}
-    //   utf[i] = UTF8.CodeUnit(0x41 + (seed + Int(x)) % 26)
-    // }
+    let options = Hasher.Options(
+      length: lengthButton.selectedTag(),
+      requireDigit: requireDigit.checked,
+      requireSpecial: requireSpecial.checked,
+      requireMixed: requireMixed.checked,
+      forbidSpecial: forbidSpecial.checked,
+      onlyDigits: onlyDigits.checked
+    )
+    return Hasher(options: options).hash(tag: tagField.stringValue, pass: passField.stringValue)
   }
 }
 
