1cc7b2237e1ff53555431b56bade627a91c08893
Nathan Dinsmore <nathan@users.noreply.github.com>
2015-07-26 02:24:22 -0400

    Use a slightly less horrifying kludge for HotKeys

--- Hush/HotKeys.swift
+++ Hush/HotKeys.swift
@@ -1,31 +1,18 @@
 import Cocoa
 import Carbon
 
-func toOSType(string: String) -> OSType {
-  var result: OSType = 0
-  if let data = string.dataUsingEncoding(NSMacOSRomanStringEncoding) {
-    let bytes = UnsafePointer<UInt8>(data.bytes)
-    for i in 0..<data.length {
-      result = result << 8 | OSType(bytes[i])
-    }
-  }
-  return result
-}
-
 class HotKeys {
-  static var handlers: [() -> ()] = []
-
   static func registerHotKey(keyCode: UInt32, modifiers: UInt32, block: () -> ()) {
-    let hotKeyID = EventHotKeyID(signature: toOSType("hush"), id: 1)
+    let hotKeyID = EventHotKeyID(signature: 1, id: 1)
     var eventType = EventTypeSpec(eventClass: OSType(kEventClassKeyboard), eventKind: UInt32(kEventHotKeyPressed))
 
-    let i = handlers.count
-    handlers.append(block)
+    let ptr = UnsafeMutablePointer<() -> ()>.alloc(1)
+    ptr.initialize(block)
 
-    InstallEventHandler(GetApplicationEventTarget(), {(_: EventHandlerCallRef, _: EventRef, index: UnsafeMutablePointer<Void>) -> OSStatus in
-      HotKeys.handlers[unsafeBitCast(index, Word.self)]()
+    InstallEventHandler(GetApplicationEventTarget(), {(_: EventHandlerCallRef, _: EventRef, ptr: UnsafeMutablePointer<Void>) -> OSStatus in
+      UnsafeMutablePointer<() -> ()>(ptr).memory()
       return noErr
-    }, 1, &eventType, UnsafeMutablePointer(bitPattern: Word(i)), nil)
+    }, 1, &eventType, ptr, nil)
     RegisterEventHotKey(keyCode, modifiers, hotKeyID, GetApplicationEventTarget(), OptionBits(0), nil)
   }
 }
